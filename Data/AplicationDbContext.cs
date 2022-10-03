0cca6487d1be9213ec51a448ea84e2111fbde87e
RodrigoPasaye <cv6200@innovaccion.mx>
2022-10-02 20:02:14 -0500

    Se agrega la primer migracion del modelo Categoria con la cual se crea la BD

--- Data/AplicationDbContext.cs
+++ Data/AplicationDbContext.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AdminIngresosGastos.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AdminIngresosGastos.Data {
     public class AplicationDbContext : DbContext {
         public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options) { }
+
+        public DbSet<Categoria> Categorias { get; set; }
     }
 }
