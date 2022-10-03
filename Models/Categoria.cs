d6204979bfdb76bad015fab182bc53bb2248aeb8
RodrigoPasaye <cv6200@innovaccion.mx>
2022-10-02 19:51:21 -0500

    Se crea el primer modelo

---
+++ Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdminIngresosGastos.Models {
+    public class Categoria {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [Display(Name ="Nombre Categoria")]
+        public string NombreCategoria { get; set; }
+        [Required]
+        [MaxLength (2)]
+        [Display(Name ="Tipo")]
+        public string Tipo { get; set; } // IN Ingreso, GA Gasto
+        [Required]
+        public bool Estado { get; set; } //True, False
+    }
+}
